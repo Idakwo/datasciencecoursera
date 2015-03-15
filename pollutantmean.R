d1e35ff824751fee7db9444e03d16f3d90a8c07c
Idakwo <Idakwo@users.noreply.github.com>
2015-03-14 22:56:54 -0500

    Update pollutantmean.R

--- pollutantmean.R
+++ pollutantmean.R
@@ -32,7 +32,7 @@
 +  }
 > 
 > 
-> pollutantmean("specdata", "sulfate", 1:10)
-[1] 4.064
-> pollutantmean("specdata", "nitrate", 20:30)
-[1] 3.881
+# tests
+pollutantmean("specdata", "sulfate", 1:10) == 4.064
+pollutantmean("specdata", "nitrate", 70:72) == 1.706
+pollutantmean("specdata", "nitrate", 23) == 1.281
